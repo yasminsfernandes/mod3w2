a9362455b967f146e04d84fbc3e0347c5dd3bd31
Yasmin Fernandes <115509004+yasminsfernandes@users.noreply.github.com>
2023-07-09 13:15:54 -0300

    Segundo comentario

--- src/main.js
+++ src/main.js
@@ -13,5 +13,5 @@ var resultado;
 
 for(let conta =1; conta <= 10; conta++) {
     resultado = conta * 2 ;
-    console.log('Tabuada do 2 = ' + conta + ' * 2 = ' + resultado)
+    console.log('Tabuada do 2  = ' + conta + ' * 2 = ' + resultado)
 }
